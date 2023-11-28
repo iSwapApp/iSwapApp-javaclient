20bf678e2d1c4176b632d6cec9cdd7f022d5df35
Turki Baatta <tbaatta@stu.kau.edu.sa>
2023-11-28 13:04:42 +0300

    Update App class close #15

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -14,7 +14,7 @@ public class App
         Scanner in = new Scanner(System.in);
         Customer c1 = new Customer();
         Card cd1 = new Card();
-        try {
+/*        try {
             DBConnection db1 = DBConnection.getInstance();
             DBConnection db2 = DBConnection.getInstance();
             if (db1 == db2) {
@@ -24,7 +24,7 @@ public class App
             }
         }catch (SQLException e){
             System.out.println(e.getMessage());
-        }
+        }**/
 
         int ID;
         byte choice;
@@ -33,10 +33,9 @@ public class App
             System.out.println("******************************");
             System.out.println("******* Welcome to App *******");
             System.out.println("******************************");
-            System.out.println("| 1.Log in (Customer)         |");
-            System.out.println("| 2.Log in (Admin)            |");
-            System.out.println("| 3.Create new account        |");
-            System.out.println("| 4.Exit                      |");
+            System.out.println("| 1.Log in                    |");
+            System.out.println("| 2.Create new account        |");
+            System.out.println("| 3.Exit                      |");
             System.out.println("******************************");
             System.out.println("Enter your choice: ");
             choice = in.nextByte();
@@ -48,14 +47,7 @@ public class App
                     customerScreen(c1, cd1, choice, in);
                 }
 
-            } else if (choice == 2) {
-
-
-                System.out.println("Enter the card code: ");
-                cd1.setCardCode(in.next());
-
-
-            } else if (choice == 3) {
+            }  else if (choice == 3) {
                 c1.newAccount();
 
             } else {
