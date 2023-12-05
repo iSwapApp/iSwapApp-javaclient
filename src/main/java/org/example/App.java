9bba5669d53836db63ae766bdaf9c50777fdf9ea
Turki Baatta <tbaatta@stu.kau.edu.sa>
2023-12-06 01:15:02 +0300

    Update ExchangeCard class

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,16 +1,13 @@
 package org.example;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Customer c1 = new Customer();
-        ExchangingCard ec = new ExchangingCard();
         Card cd1 = new Card();
-        Order order = new Order();
+        ExchangingCard order = new ExchangingCard();
 
         byte choice;
         do {
@@ -27,7 +24,7 @@ public class App {
             if (choice == 1) {
                 boolean ch = c1.check();
                 if (ch == true) {
-                    customerScreen(c1, cd1, choice, in, ec, order);
+                    customerScreen(c1, cd1, choice, in, order);
                 }
             } else if (choice == 2) {
                 c1.newAccount();
@@ -37,19 +34,18 @@ public class App {
         } while (choice != 3);
     }
 
-    public static void customerScreen(Customer c1, Card cd1, byte choice, Scanner in, ExchangingCard ec, Order order) {
+    public static void customerScreen(Customer c1, Card cd1, byte choice, Scanner in, ExchangingCard order) {
 
         System.out.println("Welcome " + c1.getName());
-        while (choice != 6) {
+        while (choice != 5) {
             System.out.println("******************************");
             System.out.println("********** iSwap App *********");
             System.out.println("******************************");
             System.out.println("| 1.Show Information         |");
             System.out.println("| 2.Show My Cards            |");
             System.out.println("| 3.Create Order             |");
-            System.out.println("| 4.Show Order List          |");
-            System.out.println("| 5.Exchange Card            |");
-            System.out.println("| 6.Log out                  |");
+            System.out.println("| 4.Exchange Card            |");
+            System.out.println("| 5.Log out                  |");
             System.out.println("******************************");
             System.out.println("Enter your choice: ");
             choice = in.nextByte();
@@ -64,15 +60,9 @@ public class App {
                 order.createOrder(c1.getID());
 
             } else if (choice == 4) {
-                order.showOrderList();
+                order.showOrderList(c1.getID());
 
             } else if (choice == 5) {
-                cd1.hanle();
-                ec.printEx(cd1);
-                cd1.setStat(new NotActiveCardState());
-                cd1.hanle();
-
-            } else if (choice == 6) {
                 System.out.println("Good Bye.");
             } else {
                 System.out.println("You enter invalid choice try again!");
--- src/main/java/org/example/ExchangingCard.java
+++ src/main/java/org/example/ExchangingCard.java
@@ -1,11 +1,123 @@
 package org.example;
 
-import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.*;
 
 public class ExchangingCard {
+    Scanner in = new Scanner(System.in);
+    Card cd1 = new Card();
 
-    public void printEx(Card cd1) {
+    //Query(SQL)
+    String query;
+    Connection conn;
+    Statement stat;
+    ResultSet result;
+    private String OrderID;
+    private String brand;
+    private int cardID;
+    private double value;
 
+    public ExchangingCard() {
+    }
 
+    public void createOrder(int ID) {
+        try {
+            System.out.println("Please insert the brand card that want add to order list: ");
+            String str = in.next();
+
+            conn = DBConnection.getInstance().getConnection();
+            stat = conn.createStatement();
+
+            //Card Table
+            query = "SELECT * FROM card WHERE brand='" + str + "'";
+            result = stat.executeQuery(query);
+
+            while (result.next()) {
+                if (ID == result.getInt("ID") && result.getString("CardValidity").equals("Active")) {
+                    cardID = result.getInt("CardID");
+                    brand = result.getString("brand");
+                    value = result.getDouble("CardValue");
+                    System.out.println("The brand is " + brand + " " + value);
+
+                    System.out.println("Do you want to insert this(Y/N)? ");
+                    str = in.next();
+
+                    System.out.println("What is the card do you want: ");
+                    String want = in.next();
+
+                    if (str.equals("Y")) {
+                        int rand = (int) (Math.random() * 1000);
+                        query = "INSERT INTO orderlist VALUES('" + rand + "','" + brand + "','" + want + "','" + value + "','" + cardID + "','" + ID + "')";
+                        stat.execute(query);
+
+                        query = "UPDATE card SET CardValidity='NotActive' WHERE CardID='" + cardID + "'";
+                        stat.execute(query);
+                        break;
+                    }
+                } else {
+                    System.out.println("Sorry, You don't have an Active card!!!");
+                }
+            }//End of while
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void showOrderList(int ID) {
+        try {
+            // query
+            conn = DBConnection.getInstance().getConnection();
+            stat = conn.createStatement();
+
+            //Card Table
+            query = "Select * from orderlist";
+            result = stat.executeQuery(query);
+
+            while (result.next()) {
+                System.out.println("The Order ID: " + result.getInt("OrderID"));
+                System.out.println("Exchange From: " + result.getString("Brand"));
+                String bx = result.getString("Want");
+                System.out.println("Exchange To: " + bx);
+                System.out.println("Card Value: " + result.getDouble("cardValue"));
+                System.out.println();
+
+                System.out.println("What order do you want : ");
+                int ord = in.nextInt();
+
+                if (ID == result.getInt("ID") && ord == result.getInt("OrderID")){
+                    System.out.println("This is your card!!! :(");
+                    break;
+                }
+
+                result.beforeFirst();
+                while (result.next()){
+                    if (ord == result.getInt("OrderID")){
+                        cardID = result.getInt("CardID"); //1
+                        query = "UPDATE card SET ID='" + ID + "' WHERE CardID='" + cardID + "'"; // ID = 20123
+                        stat.executeUpdate(query);
+
+                        query = "UPDATE card SET CardValidity='Active' WHERE CardID='" + cardID + "'";
+                        stat.executeUpdate(query);
+
+                        //result.beforeFirst();
+                        //int IDX = result.getInt("ID");
+                        int IDX = 19354;
+                        //System.out.println(IDX);
+
+                        query = "UPDATE card SET ID='" + IDX + "' WHERE Brand='" + bx + "'";
+                        stat.executeUpdate(query);
+
+                        query = "DELETE FROM orderlist WHERE OrderID='" + ord + "'";
+                        stat.executeUpdate(query);
+                        System.out.println("Done");
+                    }
+                }
+            }//End of while
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
